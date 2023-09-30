281bdf34ea0f3639060423aff14d3a586030c5b3
Anton Paskanny <anton.paskanny@gmail.com>
2023-09-30 22:57:13 +0200

    feat: add offer mock server data

---
+++ shared/types/mock-server-data.type.ts
@@ -0,0 +1,10 @@
+export type MockServerData = {
+  cities: string[];
+  titles: string[];
+  descriptions: string[];
+  previewImages: string[];
+  galleryImages: string[];
+  authors: string[];
+  emails: string[];
+  avatars: string[];
+};
