e1364db158e092d5093e827dcfb6fa4380cd7878
Anton Paskanny <anton.paskanny@gmail.com>
2023-10-07 21:24:23 +0200

    feat: update rest app

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,9 +1,12 @@
 import PinoLogger from '../shared/libs/logger/pino.logger.js';
 import { RestApplication } from './rest/index.js';
+import { RestConfig } from '../shared/libs/config/index.js';
 
 function bootstrap() {
   const logger = new PinoLogger();
-  const restApplication = new RestApplication(logger);
+  const config = new RestConfig(logger);
+
+  const restApplication = new RestApplication(logger, config);
 
   restApplication.init();
 }
