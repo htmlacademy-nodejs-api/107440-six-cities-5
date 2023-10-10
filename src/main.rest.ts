96aa6d26ce1177ad88f8953486a092fd90027861
Anton Paskanny <anton.paskanny@gmail.com>
2023-10-10 23:01:14 +0200

    feat: update main rest

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,32 +1,21 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { Logger, PinoLogger } from '../shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
-import { Config, RestConfig, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
-import {
-  DatabaseClient,
-  MongoDatabaseClient
-} from '../shared/libs/database-client/index.js';
+import { createRestApplicationContainer } from './rest/rest.container.js';
+import { createUserContainer } from '../shared/modules/user/index.js';
+import { createCityContainer } from '../shared/modules/city/index.js';
+import { createRentOfferContainer } from '../shared/modules/rent-offer/index.js';
 
 function bootstrap() {
-  const container = new Container();
+  const appContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer(),
+    createCityContainer(),
+    createRentOfferContainer()
+  );
 
-  container
-    .bind<RestApplication>(Component.RestApplication)
-    .to(RestApplication)
-    .inSingletonScope();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container
-    .bind<Config<RestSchema>>(Component.Config)
-    .to(RestConfig)
-    .inSingletonScope();
-  container
-    .bind<DatabaseClient>(Component.DatabaseClient)
-    .to(MongoDatabaseClient)
-    .inSingletonScope();
-
-  const restApplication = container.get<RestApplication>(
+  const restApplication = appContainer.get<RestApplication>(
     Component.RestApplication
   );
   restApplication.init();
