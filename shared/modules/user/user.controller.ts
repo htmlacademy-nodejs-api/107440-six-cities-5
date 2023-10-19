4f04720877d0330556a185c5a54bf3820aa2db0c
Anton Paskanny <anton.paskanny@gmail.com>
2023-10-19 19:19:37 +0200

    feat: update user controller

--- shared/modules/user/user.controller.ts
+++ shared/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'inversify';
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { BaseController, HttpMethod } from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { Component } from '../../types/index.js';
@@ -12,13 +12,17 @@ export class UserController extends BaseController {
     this.logger.info('Register routes for UserController…');
 
     this.addRoute({
-      path: '/register',
+      path: '/signup',
       method: HttpMethod.Post,
       handler: this.create
     });
   }
 
-  public create(_req: CreateUserRequest, _res: Response): void {
+  public async create(
+    _req: CreateUserRequest,
+    _res: Response,
+    _next: NextFunction
+  ): Promise<void> {
     throw new Error('[UserController] Oops');
   }
 }
