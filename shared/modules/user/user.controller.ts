74c20bdd88aaada4c65cc3bfcaf1c7839514f14b
Anton Paskanny <anton.paskanny@gmail.com>
2023-10-22 11:24:42 +0200

    feat: fix small issues

--- shared/modules/user/user.controller.ts
+++ shared/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'inversify';
-import { NextFunction, Response } from 'express';
+import { Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import {
   BaseController,
